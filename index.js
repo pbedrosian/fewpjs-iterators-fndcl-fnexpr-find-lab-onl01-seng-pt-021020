ff74cee182ccf2923aaa1bea7ce60ef459336e8f
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-01 21:27:35 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(season) {
+  let test = season.find(function(s) {return s.result === 'W'})
+  if (test === undefined) {
+    return undefined
+  } else {
+    return test.year
+  }
+}
